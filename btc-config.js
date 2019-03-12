2c0997c963dfef1917d536dee8ee374cd3edc96b
Stepan Mytchyk <st.mytchyk@gmail.com>
2019-03-12 14:05:08 +0200

    Added google sheets integration

--- btc-config.js
+++ btc-config.js
@@ -240,6 +240,14 @@ config.redisBeacon = {
   ]
 }
 
+config.gforms = {
+  enabled: false,
+  botTag: 'btc-live-v3', //Add a custom tag here. This will be included in the name of the spreadsheet tab for this bot.
+  // Get a prefilled link of your google for, each question answered with a single space and paste here.
+  // It should resemble: https://docs.google.com/forms/d/e/1FAIp-My-Form-ID-K6PaOg3bPLg/viewform?usp=pp_url&entry.852051357=+&entry.1346916648=+&entry.1743858251=+&entry.105864059=+&entry.68010386=+&entry.3616735=+&entry.1463011579=+&entry.433943481=+&entry.620326103=+&entry.1202282384=+&entry.1415514787=+
+  prefill: 'https://docs.google.com/forms/d/e/1FAIpQLSd_NjKn0mC6vTTYh75-XC0UeDGei76HNRaTAR47As93ZSdzqg/viewform?usp=pp_url&entry.882271813=+&entry.1578565422=+&entry.492078904=+&entry.106640264=+&entry.461285708=+&entry.722568253=+&entry.1698569090=+&entry.270661511=+&entry.7268994=+&entry.899582966=+&entry.78055483=+',
+};
+
 config.slack = {
   enabled: false,
   token: '',
